630b3875a0a569da28d53db1ddba94b75481c44c
Robert Will <matey.jack@gmail.com>
2025-07-28 09:16:42 +0200

    WIP: infrastruktur

--- src/main/java/Gleisharfe.java
+++ src/main/java/Gleisharfe.java
@@ -1,7 +1,22 @@
-import java.util.List;
+import de.tuberlin.bbi.dr.LayoutController;
 
-public interface Gleisharfe {
-    List<Integer> getAllTrackIds();
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public abstract class Gleisharfe {
+    Map<Integer, Set<FahrwegElement>> fahrwegKonfiguration;
+
+    public Gleisharfe(Map<Integer, Set<FahrwegElement>> fahrwegKonfiguration) {
+        this.fahrwegKonfiguration = fahrwegKonfiguration;
+    }
+
+    public List<Integer> getAllTrackIds() {
+        return fahrwegKonfiguration.keySet().stream().toList();
+    }
+
+    public abstract boolean initialize();
 
     /**
      * Fahrweg zum Gleis einstellen und alle Weichen prüfen.
@@ -9,6 +24,23 @@ public interface Gleisharfe {
      * @param trackId - Gleis in der Harfe zu dem es gehen soll.
      * @return true bei Erfolgreicher Einstellung und Prüfung.
      */
-    boolean setFahrwegToTrack(int trackId);
+    public boolean setFahrwegToTrack(int trackId) {
+        var points = fahrwegKonfiguration.get(trackId);
+        for (FahrwegElement point : points) {
+            LayoutController.turnoutByAddress(point.turnoutId())
+                    .setPosition(point.turnoutPosition());
+        }
+        try {
+            TimeUnit.SECONDS.sleep(1);
+            for (FahrwegElement point : points) {
+                if (LayoutController.turnoutByAddress(point.turnoutId()).getPosition() != point.turnoutPosition()) {
+                    TimeUnit.SECONDS.sleep(1);
+                }
+            }
+            return true;
+        } catch (InterruptedException e) {
+            return false;
+        }
+    }
 
 }
--- src/main/java/Infrastruktur_SG.java
+++ src/main/java/Infrastruktur_SG.java
@@ -1,48 +1,57 @@
 import de.tuberlin.bbi.dr.LayoutController;
+import de.tuberlin.bbi.dr.Turnout.Position;
 
-import java.util.List;
 import java.util.Map;
-
-import static de.tuberlin.bbi.dr.Turnout.Position.CLOSED;
-import static de.tuberlin.bbi.dr.Turnout.Position.THROWN;
+import java.util.Set;
 
 public class Infrastruktur_SG {
-    public static class LinkeSeite implements Gleisharfe {
-        public static final int W1 = 381;
-        public static final int W2 = 380;
-        private static final int W3 = 380;
-        private static final int W4 = 380;
+    public static final int W1 = 381;
+    public static final int W2 = 380;
+    private static final int W3 = 383;
+    private static final int W4 = 382;
 
-        // now that could be read from a CSV file ...
-        Map<Integer, FahrwegElement> fahrwegKonfiguration;
-
-        public void initialize(LayoutController controller) {
+    public static Gleisharfe LinkeSeite = new Gleisharfe(Map.of(
+            1, Set.of(new FahrwegElement(W1, Position.CLOSED)),
+            2, Set.of(
+                    new FahrwegElement(W1, Position.THROWN),
+                    new FahrwegElement(W2, Position.CLOSED)
+            )
+    )) {
+        boolean initialize() {
             var w3 = LayoutController.turnoutByAddress(W3);
-            w3.setPosition(CLOSED);
+            w3.setPosition(Position.CLOSED);
             var w4 = LayoutController.turnoutByAddress(W4);
-            w4.setPosition(CLOSED);
+            w4.setPosition(Position.CLOSED);
+            return true;
         }
+    };
 
-        @Override
-        public List<Integer> getAllTrackIds() {
-            return List.of(1, 2, 3, 4);
-        }
+    public static final int W11 = 378;
+    public static final int W12 = 384;
+    public static final int W13 = 379;
+    private static final int W14 = 371;
 
-        @Override
-        public boolean setFahrwegToTrack(int trackId) {
-            return false;
-        }
-    }
-
-    public static class RechteSeite {
-        public static final int W11 = 378;
-        public static final int W12 = 384;
-        public static final int W13 = 379;
-        private static final int W14 = 371;
-
-        public void initialize(LayoutController controller) {
+    public static Gleisharfe RechteSeite = new Gleisharfe(Map.of(
+            2, Set.of(
+                    new FahrwegElement(W11, Position.THROWN),
+                    new FahrwegElement(W12, Position.CLOSED)
+            ),
+            3, Set.of(
+                    new FahrwegElement(W11, Position.THROWN),
+                    new FahrwegElement(W12, Position.THROWN),
+                    new FahrwegElement(W13, Position.CLOSED)
+            ),
+            4, Set.of(
+                    new FahrwegElement(W11, Position.THROWN),
+                    new FahrwegElement(W12, Position.THROWN),
+                    new FahrwegElement(W13, Position.THROWN)
+            )
+    )) {
+        public boolean initialize() {
             var w14 = LayoutController.turnoutByAddress(W14);
-            w14.setPosition(THROWN);
+            w14.setPosition(Position.THROWN);
+            return true;
         }
-    }
+    };
 }
+
